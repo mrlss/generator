d37e67a8fcd4e5be23c168a6263ec0322df99ef1
alina.golovko <christian@dreamproduction.com>
2018-07-18 13:47:00 +0300

    v.0.1

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+import test from './vendors/vendorTest';
+import bgVideo from './vendors/bgVideo';
+
+test;
+console.log('test1');
+console.log('test1');
+console.log('test1');
+console.log('test1');
+console.log('test1');
+console.log('test1');
+console.log('test1');
+console.log('test1');
+console.log('test1');
+console.log('test1');
+console.log('test1');
+
+
